f2299b358b1d12f4b47e3f88c272f18cf1ae2d04
MrBonesYk <167470642+MrBonesYk@users.noreply.github.com>
2024-05-05 13:03:11 -0600

    Add files via upload

---
+++ TextToSpeechDemo/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech;
+using System.IO;
+
+namespace TextToSpeechDemo
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speech;
+        public Form1()
+        {
+            InitializeComponent();
+            speech = new SpeechSynthesizer();
+        }
+        // Original code by: Original code by: C# Ui Academy
+        // https://www.youtube.com/watch?v=aGtKyZ4AFcw
+
+        // Alerts, file saving, button to clear the text box, and voice selection added by MrBones (Me).
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var voice in speech.GetInstalledVoices())
+            {
+                comboBox1.Items.Add(voice.VoiceInfo.Name);
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                speech.SelectVoice(comboBox1.Text);
+                speech.SpeakAsync(richTextBox1.Text);
+                this.Alert("Speaking", Form_Alert.enmType.Success);
+            }
+            else
+            {
+                this.Alert("Pls write something", Form_Alert.enmType.Error);
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (speech.State == SynthesizerState.Speaking)
+            {
+                speech.Pause();
+                this.Alert("Paused", Form_Alert.enmType.Warning);
+            }
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (speech.State == SynthesizerState.Paused)
+            {
+                speech.Resume();
+                this.Alert("Continuing", Form_Alert.enmType.Info);
+            }
+        }
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Wave File|*.wav";
+            saveFileDialog.Title = "Save Speech as Wave File";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                speech.SetOutputToWaveFile(saveFileDialog.FileName);
+                speech.Speak(richTextBox1.Text);
+                speech.SetOutputToDefaultAudioDevice();
+                this.Alert("Saved!", Form_Alert.enmType.Info);
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            this.Alert("Text Box Cleaned", Form_Alert.enmType.Info);
+        }
+        public void Alert(string msg, Form_Alert.enmType type)
+        {
+            Form_Alert frm = new Form_Alert();
+            frm.showAlert(msg, type);
+        }
+    }
+}
---
+++ TextToSpeechDemo/Form_Alert.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TextToSpeechDemo
+{
+    partial class Form_Alert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Alert));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Green;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(87, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.Location = new System.Drawing.Point(93, 27);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(46, 24);
+            this.lblMsg.TabIndex = 1;
+            this.lblMsg.Text = "Msg";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TextToSpeechDemo.Properties.Resources.success;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TextToSpeechDemo.Properties.Resources.error;
+            this.pictureBox2.Location = new System.Drawing.Point(358, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form_Alert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(390, 83);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_Alert";
+            this.Text = "Form_Alert";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
